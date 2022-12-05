463e6e61f621de8fdadff35d5e779882d01cd13e
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-12-05 23:17:48 +0200

    дополнительные поправки по дз

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -12,18 +12,21 @@ const ingredients = [
 
 
 const listWithIngr = document.querySelector("#ingredients");
+
 console.log(listWithIngr);
 
+const liArray = [];
 
-  ingredients.forEach(ingredient => {
+
+ingredients.forEach(ingredient => {
 
   const liForList = document.createElement("li");
   liForList.textContent = ingredient;
   liForList.classList.add("item");
 
-return liForList; 
+liArray.push(liForList); 
 
 })
 
-listWithIngr.append(...liForList);
+listWithIngr.append(...liArray);
 
